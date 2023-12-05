25e6cbdbfd42181532b8a05316dea9747b7ee7ae
xgppp <152986835+xgppp@users.noreply.github.com>
2023-12-05 21:52:12 +0800

    方块

--- main.ts
+++ main.ts
@@ -1,1 +1,9 @@
-
+namespace fun {
+    /**
+    * 计算著名的斐波那契数列！
+    */
+    //% block
+    export function fib(value: number): number {
+        return value <= 1 ? value : fib(value - 1) + fib(value - 2);
+    }
+}
